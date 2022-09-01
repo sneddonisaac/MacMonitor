f1537b707a88133074f774886e43af3ecc909f54
Isaac Sneddon-Jenkins <hello@isaacsneddonjenkins.co.uk>
2022-09-01 20:39:46 +0100

    Initial Commit

---
+++ MacMonitor/MacMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMonitorApp.swift
+//  MacMonitor
+//
+//  Created by Isaac Sneddon-Jenkins on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MacMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
