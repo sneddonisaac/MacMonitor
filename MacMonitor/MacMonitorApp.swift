369c9756905254ebcb0cca331ba964d2479827de
Isaac Sneddon-Jenkins <hello@isaacsneddonjenkins.co.uk>
2022-09-01 21:14:42 +0100

    Initial Commit

--- MacMonitor/MacMonitorApp.swift
+++ MacMonitor/MacMonitorApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct MacMonitorApp: App {
+    @AppStorage("showMenuBarExtra") private var showMenuBarExtra = true
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        MenuBarExtra(
+            "App Menu Bar Extra", systemImage: "star",
+            isInserted: $showMenuBarExtra)
+        {
+            Text("Test")
         }
     }
 }
